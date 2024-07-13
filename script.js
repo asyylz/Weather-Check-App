2f38575bc900de63e069ce0c63997a13a50af8b1
asyylz <49663592+asyylz@users.noreply.github.com>
2024-07-13 19:58:17 +0100

    "Updated script.js: replaced double quotes with single quotes, updated API key to use environment variable, and made minor formatting changes."

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 /* -------------------- DOM variables ------------------- */
-const citySearchInput = document.querySelector("input.mb-5");
-const searchIcon=document.querySelector(".fa.fa-search");
-console.log(searchIcon)
-const cityName = document.querySelector("h2");
-const weatherDegree = document.querySelector("p.large-font");
-const time = document.querySelector("p.time");
-const date = document.querySelector("p.date");
-const icontext = document.querySelector("p.icon-text");
-const icon = document.querySelector("img.icon");
-const cloudy = document.querySelector(".cloud p.ml-auto");
-const wind = document.querySelector(".wind p.ml-auto");
-const humidity = document.querySelector(".humidity p.ml-auto");
-const pressure = document.querySelector(".pressure p.ml-auto");
-const optCity1 = document.querySelector(".optcity.one");
-const optCity2 = document.querySelector(".optcity.two");
-const optCity3 = document.querySelector(".optcity.three");
-const optCity4 = document.querySelector(".optcity.four");
-const container = document.querySelector(".container");
-const locationIcon = document.getElementById("location");
+const citySearchInput = document.querySelector('input.mb-5');
+const searchIcon = document.querySelector('.fa.fa-search');
+console.log(searchIcon);
+const cityName = document.querySelector('h2');
+const weatherDegree = document.querySelector('p.large-font');
+const time = document.querySelector('p.time');
+const date = document.querySelector('p.date');
+const icontext = document.querySelector('p.icon-text');
+const icon = document.querySelector('img.icon');
+const cloudy = document.querySelector('.cloud p.ml-auto');
+const wind = document.querySelector('.wind p.ml-auto');
+const humidity = document.querySelector('.humidity p.ml-auto');
+const pressure = document.querySelector('.pressure p.ml-auto');
+const optCity1 = document.querySelector('.optcity.one');
+const optCity2 = document.querySelector('.optcity.two');
+const optCity3 = document.querySelector('.optcity.three');
+const optCity4 = document.querySelector('.optcity.four');
+const container = document.querySelector('.container');
+const locationIcon = document.getElementById('location');
 
 /* ------------------------- API ------------------------ */
 let cityData = [];
 let currentSunRise, currentSunSet;
-const apiKey = "b8b2bc6cc3def4c8452b6812772a682f";
-const defaultCity = "London";
+const apiKey = process.env.GEOAPIFY_API_KEY;
+const defaultCity = 'London';
 async function fetchWeatherData(city) {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
     );
     if (!response.ok) {
-      throw new Error("There was an error with the URL");
+      throw new Error('There was an error with the URL');
     }
     const data = await response.json();
     const { timezone, sys } = data;
@@ -41,7 +41,7 @@ async function fetchWeatherData(city) {
     );
     retriveWeatherData(data);
   } catch (error) {
-    console.error("An error occurred:", error);
+    console.error('An error occurred:', error);
   }
 }
 
@@ -52,31 +52,31 @@ function changeBackgroundColor(sunRiseAndSet) {
   const isNightTime = localTime < sunrise || localTime > sunset;
   if (isNightTime) {
     container.style.backgroundImage = 'url("./assets/night.jpg")';
-    container.classList.remove("day-theme");
+    container.classList.remove('day-theme');
   } else {
     container.style.backgroundImage = 'url("./assets/day.jpg")';
-    container.classList.add("day-theme");
+    container.classList.add('day-theme');
   }
 }
 /* ---------------------- Listeners --------------------- */
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   fetchWeatherData(defaultCity);
   optionalCities();
 });
 
-citySearchInput.addEventListener("keydown", function (e) {
-  if (e.keyCode === 13){
+citySearchInput.addEventListener('keydown', function (e) {
+  if (e.keyCode === 13) {
     e.preventDefault();
     let city = e.target.value;
     fetchWeatherData(city);
   }
 });
-searchIcon.addEventListener("click", function (e) {
+searchIcon.addEventListener('click', function (e) {
   let city = citySearchInput.value;
   fetchWeatherData(city);
 });
-locationIcon.addEventListener("click", function (e) {
-  const units = "metric";
+locationIcon.addEventListener('click', function (e) {
+  const units = 'metric';
   navigator.geolocation?.getCurrentPosition(async ({ coords }) => {
     // Using async here
     const { latitude, longitude } = coords;
@@ -85,7 +85,7 @@ locationIcon.addEventListener("click", function (e) {
       const response = await axios(URL); // Using await here
       retriveWeatherData(response.data);
     } catch (e) {
-      console.error("ERROR");
+      console.error('ERROR');
     }
   });
 });
@@ -94,8 +94,8 @@ locationIcon.addEventListener("click", function (e) {
 function retriveWeatherData(citySelected) {
   const { main, timezone, clouds, wind, weather, name } = citySelected;
   cityName.textContent = name;
-  weatherDegree.textContent = main.temp.toFixed(0) + "°C";
-  time.textContent = formatTimeAndDate(timezone).slice(0,5);
+  weatherDegree.textContent = main.temp.toFixed(0) + '°C';
+  time.textContent = formatTimeAndDate(timezone).slice(0, 5);
   date.textContent = formatTimeAndDate(timezone).slice(5);
   icontext.textContent = weather[0].description;
   icon.src = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
@@ -105,7 +105,7 @@ function retriveWeatherData(citySelected) {
   pressure.textContent = `${main.pressure.toFixed(0)} hPa`;
 }
 
-const optionalCitiesArray = ["Birmingham", "Manchester", "New York", "Prague"];
+const optionalCitiesArray = ['Birmingham', 'Manchester', 'New York', 'Prague'];
 async function optionalCities() {
   for (const city of optionalCitiesArray) {
     try {
@@ -126,17 +126,17 @@ async function optionalCities() {
 function displayOptionalCityData(citySelected) {
   const { main, name } = citySelected;
   switch (name) {
-    case "Birmingham":
-      optCity1.textContent = main.temp.toFixed(0) + "°C";
+    case 'Birmingham':
+      optCity1.textContent = main.temp.toFixed(0) + '°C';
       break;
-    case "Manchester":
-      optCity2.textContent = main.temp.toFixed(0) + "°C";
+    case 'Manchester':
+      optCity2.textContent = main.temp.toFixed(0) + '°C';
       break;
-    case "New York":
-      optCity3.textContent = main.temp.toFixed(0) + "°C";
+    case 'New York':
+      optCity3.textContent = main.temp.toFixed(0) + '°C';
       break;
-    case "Prague":
-      optCity4.textContent = main.temp.toFixed(0) + "°C";
+    case 'Prague':
+      optCity4.textContent = main.temp.toFixed(0) + '°C';
       break;
     default:
       break;
@@ -144,9 +144,9 @@ function displayOptionalCityData(citySelected) {
 }
 
 function formatTimeComponents(date) {
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
   return `${hours}:${minutes}:${seconds}`;
 }
 function formatTimeAndDate(timezoneOffsetMs) {
@@ -158,39 +158,39 @@ function formatTimeAndDate(timezoneOffsetMs) {
   const minutes = timeInSpecifiedTimezone.getUTCMinutes();
 
   const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
   ];
   const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
   ];
   const dayOfWeek = days[timeInSpecifiedTimezone.getUTCDay()];
   const dayOfMonth = String(timeInSpecifiedTimezone.getUTCDate()).padStart(
     2,
-    "0"
+    '0'
   );
   const monthName = months[timeInSpecifiedTimezone.getUTCMonth()];
   //const year = String(timeInSpecifiedTimezone.getUTCFullYear());
 
-  const formattedDateTime = `${hours.toString().padStart(2, "0")}:${minutes
+  const formattedDateTime = `${hours.toString().padStart(2, '0')}:${minutes
     .toString()
-    .padStart(2, "0")}${dayOfWeek}-${dayOfMonth} ${monthName}`;
+    .padStart(2, '0')}${dayOfWeek}-${dayOfMonth} ${monthName}`;
   return formattedDateTime;
 }
 
